f4c9d2d5f8471b79b34bab03593cf9eb21bfd094
PandaRouge9 <ambroise.maire7@gmail.com>
2023-06-27 12:01:24 +0200

    chore: Mise en place de Webpack
    Mise en place de l'environnement webpack

---
+++ webpack.config.js
@@ -0,0 +1,127 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'app'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+        liveReload: true,
+        port: 3000,
+        static: {
+            directory: path.join(__dirname, "app"),
+          },
+        hot: false,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            meta: {
+                viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+                description: "C'est la famille !",
+            },
+            template: './src/index.html',
+            filename: '../app/index.html',
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html?$/i,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        // options: { minimize: false }
+                    },
+                ]
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: {"ie": "11"}}]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [stylesHandler, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: 'assets/[name][ext]',
+                },
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new ImageMinimizerPlugin({
+                generator: [
+                    {
+                        preset: "webp",
+                        implementation: ImageMinimizerPlugin.sharpGenerate,
+                        options: {
+                            encodeOptions: {
+                                webp: {
+                                    quality: 80,
+                                },
+                            },
+                        },
+                    },
+                ],
+            }),
+          ],
+    }
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
